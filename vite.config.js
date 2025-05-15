e16be09666f1d0bc048c23ef7b85fc5176865349
rishabh tekam <rishabhtekam8@gmail.com>
2025-05-15 13:30:25 +0530

    fix: rewrite all routes to index.html for SPA routing

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    build: {
+    chunkSizeWarningLimit: 1000,
+  },
+    "rewrites": [
+    { "source": "/(.*)", "destination": "/index.html" }
+  ],
 })
 
